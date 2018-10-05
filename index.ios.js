dbe178779d4c6c9fe782f45e17c1a452e62bc40b
arevaldez <valdezalex31@gmail.com>
2018-10-05 15:57:36 -0500

    Edited index.js to index.ios.js

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+// Index.ios.js 
+
+// Import a library to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a component
+
+
+// Render it to the device
