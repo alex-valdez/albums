c0a367a3e4ae0ee269ffe4af91bc89e91a581039
arevaldez <valdezalex31@gmail.com>
2018-10-19 12:58:48 -0500

    Added AlbumList

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+
+    state = {
+        albums: [],
+    };
+
     componentWillMount() {
-        fetch('https://rallycoding.herokuapp.com/api/music_albums')
-        .then(resp => console.log(resp));
+        fetch('http://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => console.log(response.data));
     }
 
     render() {
